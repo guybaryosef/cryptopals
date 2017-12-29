9d2ae9a4eb4bad10c630a24f0dc9a5da07609043
Guy Bar Yosef <guybaryosef@gmail.com>
2017-12-29 17:23:10 +0200

    update to challenge1
    made the file into two complete steps, hexadecimal to binary & binary to base64; will allow for an easier time using it in future challenges.

--- set1/c1_HexToBase64.cpp
+++ set1/c1_HexToBase64.cpp
@@ -2,11 +2,10 @@
  Cryptopals Crypto Challenges
  Set 1
  Challenge 1 - Convert hex to base64
- Version 1.0
+ Version 1.1
 
  By: Guy Bar Yosef
  */
-
 
 #include <string>
 #include <cmath>
@@ -14,32 +13,40 @@
 
 using namespace std;
 
-string hex2bin (char hex);  //function converting hexadecimal values to binary
-string ENCODING64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"; // base64 encoding scheme
+string hex2bin (string hex);     // converts hexadecimal values to binary
+string bin2base64 (string bin);  // converts binary to base64
 
 int main () {
 
     string hex;             // inputted hex value
-    string bin = "";        //intermediate binary value
-    string base64 = "";     //output base64 value
     cin >> hex;
 
-    int hexlen = hex.length();
-    int binlen = hexlen*4;                          //total binary value length
+    string bin = hex2bin(hex);          //intermediate binary value
+    string base64 = bin2base64(bin);    //output base64 value
 
-    for (int i = 0 ; i <= hexlen ; i++)             //converting hex to binary through hex2bin function
-        bin.append( hex2bin(hex[i]) );
+    cout << base64 << endl;
+    return 0;
+}
 
-    for (int i = binlen - 1 ; i >= 0 ; i -= 6) {    //iterate through 6-bit values, right to left
-        string current;                             // the 6 bit base64 value
-        if (i < 5) {                                // at final iteration, final base64 value could be < 6 bits
+/*
+ * converts binary digit to base64
+ */
+string bin2base64(string bin) {
+
+    string base64 = "";     //final base64 value
+    int binlen = bin.length();
+    string ENCODING64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"; // base64 encoding scheme
+
+    for (int i = binlen - 1 ; i >= 0 ; i -= 6) {    //iterate through binary digit, at 6-bit value intervals, right to left
+        string current;                             // the 6-bit value in decimal
+        if (i < 5) {                                // dealing w/ final iteration, final base64 value could be < 6 bits
             int bufzeroscount = 5 - i;              // amount of 0s to add to make 'current' a 6-bit value
             string bufzero = "0";
             for (int j = bufzeroscount ; j > 1 ; j--)
                 bufzero.append("0");
-            current = bufzero + bin.substr(0, i + 1); //combining
+            current = bufzero + bin.substr(0, i + 1); // the final 6-bit digit, w/ zero buffer
         }
-        else                                        // current will be a 6-bit value
+        else                                        // all other iteration except maybe final iteration
             current = bin.substr(i - 5, 6);
 
         int value = 0;
@@ -47,31 +54,35 @@ int main () {
             value += (current[j] == '0') ? 0 : pow(2, k);
         base64 = ENCODING64[value] + base64;
     }
-
-    cout << base64 << endl;
-    return 0;
+    return base64;
 }
 
 /*
- * Uses a switch statement to convert a hexadecimal value to its 4 bit binary equivalent
+ * converts hexadecimal digit to binary
  */
-string hex2bin (char hex) {
-    switch(hex) {
-        case '0': return "0000";
-        case '1': return "0001";
-        case '2': return "0010";
-        case '3': return "0011";
-        case '4': return "0100";
-        case '5': return "0101";
-        case '6': return "0110";
-        case '7': return "0111";
-        case '8': return "1000";
-        case '9': return "1001";
-        case 'a': return "1010";
-        case 'b': return "1011";
-        case 'c': return "1100";
-        case 'd': return "1101";
-        case 'e': return "1110";
-        case 'f': return "1111";
-        }
+string hex2bin (string hex) {
+
+    string bin = "";            //final binary value
+    int hexlen = hex.length();
+
+    for (int i = 0 ; i < hexlen ; i++)     //converting hex to binary through 
+        switch(hex[i]) {
+            case '0': bin.append("0000"); break;
+            case '1': bin.append("0001"); break;
+            case '2': bin.append("0010"); break;
+            case '3': bin.append("0011"); break;
+            case '4': bin.append("0100"); break;
+            case '5': bin.append("0101"); break;
+            case '6': bin.append("0110"); break;
+            case '7': bin.append("0111"); break;
+            case '8': bin.append("1000"); break;
+            case '9': bin.append("1001"); break;
+            case 'b': bin.append("1011"); break;
+            case 'c': bin.append("1100"); break;
+            case 'd': bin.append("1101"); break;
+            case 'a': bin.append("1010"); break;
+            case 'e': bin.append("1110"); break;
+            case 'f': bin.append("1111"); break;
+            }
+    return bin;
 }
