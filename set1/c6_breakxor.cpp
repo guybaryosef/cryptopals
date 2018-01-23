efb297278f4f2b947dcc76f78dc7c182bb11f5e1
Guy Bar Yosef <guybaryosef@gmail.com>
2018-01-23 00:33:16 -0500

    Create c6_breakxor.cpp
    Just the skeleton of the challenge, haven't actually begun anything

---
+++ set1/c6_breakxor.cpp
@@ -0,0 +1,28 @@
+/*
+ Cryptopals Crypto Challenges
+ Set 1
+ Challenge 6 - Break repeating-key XOR
+ Version 1.0
+
+ Comment: Requires C++11 or later
+
+ By: Guy Bar Yosef
+ */
+
+
+int hammingDistance(string one, string two);
+
+int main() {
+
+
+}
+
+
+/*
+ *  This funciton computes the Hamming Distance between two strings
+ *  Input is the two strings to compare and the output is the number of differing bits between the two.
+ */
+int hammingDistance(string one, string two) {
+    
+
+}
