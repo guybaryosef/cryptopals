cf171b13e377ed5aaca48858f537ee769d00ce99
Guy Bar Yosef <guybaryosef@gmail.com>
2018-01-31 11:10:09 -0500

    Update c5_repeatedxor.cpp
    - Added option to choose key to encrypt with.

--- set1/c5_repeatedxor.cpp
+++ set1/c5_repeatedxor.cpp
@@ -2,7 +2,7 @@
  Cryptopals Crypto Challenges
  Set 1
  Challenge 5 - Repeating-key XOR
- Version 1.0
+ Version 1.1
 
  Comment: Requires C++11 or later
  By: Guy Bar Yosef
@@ -14,13 +14,16 @@
 #include "c2_fixedXOR_1_0.cpp"
 
 
-string repeatingICExor(string input);   //encrypts a string using repeated-key XOR with key "ICE"
+string repeatingxor(string inputfile, string key);   //encrypts a string using repeated-key XOR with key
 
 /*
 int main() {
-    string input;
-    cin >>input;
-    cout << bintohex(repeatingICExor(input)) << endl;  //printed as hex due to challenge specs
+    string input, key;
+    cout << "File to encrypt: " << fulsh;
+    cin >> input;
+    cout << "Key: " << flush;
+    cin >> key;
+    cout << bintobase64( repeatingxor(input, key) ) << endl;  //printed as binary digit string
     return 0;
 }
 */
@@ -29,10 +32,8 @@ int main() {
 /*
  *  Takes a text file as input and returns the contents of that file XORed repeatedly with the key "ICE".
  */
-string repeatingICExor(string input) {
-    string key = "ICE";             // our specified key
-
-    ifstream input_file(input);     // this constructor requires C++11 or heigher to compile
+string repeatingxor(string inputfile, string key) {
+    ifstream input_file(inputfile);     // this constructor requires C++11 or heigher to compile
     assert( input_file.is_open() && "File could not be found :(" );
 
     string encrypted;       // output
@@ -41,9 +42,9 @@ string repeatingICExor(string input) {
     input_file >> noskipws >> c;    //reads char by char not skipping whitespace
     while ( !input_file.eof() ) {
         //convert the chars to their respective string of bytes and then XORs them together
-        std::bitset<8> bytekey(key[count % 3]);   //cyclcly goes through I,C,E,I,C,...
+        std::bitset<8> bytekey(key[count % key.length()]);   //cyclcly goes through I,C,E,I,C,...
         std::bitset<8> bytec(c);
-        encrypted.append(twoBinXor( bytekey.to_string(), bytec.to_string()  ));
+        encrypted.append(twoBinXor( bytekey.to_string(), bytec.to_string() ));
         count++;            //serves to go to the next letter in 'key'
         input_file >> noskipws >> c;
     }
