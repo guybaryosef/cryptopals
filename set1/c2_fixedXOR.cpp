d3bcbd67b69f0daa70f7f72117cc7ffb474f1a4d
Guy Bar Yosef <guybaryosef@gmail.com>
2018-01-31 11:04:48 -0500

    Update c2_fixedXOR.cpp
    - Changed 'twoBinXor' function to accept binary inputs instead of hexadecimal.
    - Also deals with unequally-sized inputs by buffering the shorter one with '0's.

--- set1/c2_fixedXOR.cpp
+++ set1/c2_fixedXOR.cpp
@@ -2,32 +2,44 @@
  Cryptopals Crypto Challenges
  Set 1
  Challenge 2 - Fixed XOR
- Version 1.0
+ Version 1.1
 
  By: Guy Bar Yosef
  */
 
 #include "c1_HextoBase64.cpp"
 
-string twoBinXor(string hex1, string hex2);  //XORs two hexadecimal strings
-
+string twoBinXor(string bin1, string bin2);  //XORs two binary strings
 
 /*
- *  takes two equal-length buffers and outputs their XOR combination
- */
-string twoBinXor( string hex1, string hex2) {
-    string bin1 = hextobin(hex1);           //converts from hexadecimal to binary
+int main() {
+    string hex1, hex2;
+    cin >> hex1 >> hex2;
+    string bin1 = hextobin(hex1);
     string bin2 = hextobin(hex2);
+    cout << twoBinXor(hex1, hex2) << endl;
+    return 0;
+}
+*/
 
-    string output = "";
-    int len = bin1.length();
+/*
+ * takes two equal-length buffers and outputs their XOR combination
+ */
+string twoBinXor(string bin1, string bin2) {
+    if (bin1.length() != bin2.length()) {   // buffers the shorter input with '0's
+        int difference = abs((int)bin1.length() - (int)bin2.length() );
+        if (bin1.length() > bin2.length())
+            bin1 = bin1.substr(0, bin1.length() - difference);
+        else
+            bin2 = bin2.substr(0, bin2.length() - difference);
+    }
 
-    for (int i = 0 ; i < len ; i++) {       //XORs each bit in the two buffers
+    string output;
+    for (int i = 0 ; i < bin1.length() ; i++) {   //XORs each bit
         if ( bin1[i] != bin2[i] )
             output.append("1");
         else
             output.append("0");
     }
-
-    return bintohex(output);               // returns the hexadecimal value of the binary XOR combo
+    return output;      // returns the binary value of the binary XOR combo
 }
