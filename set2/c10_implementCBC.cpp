61a845f76519b61d2cd2c8cfe7a960bd5154b6c6
Guy Bar Yosef <guybaryosef@gmail.com>
2018-05-31 16:14:09 -0400

    updated and reorganized code of the set 1

---
+++ set2/c10_implementCBC.cpp
@@ -0,0 +1,9 @@
+/*
+ Cryptopals Crypto Challenges
+ Set 2
+ Challenge 10 - Implement CDC mode
+ Version 1.0
+
+ By: Guy Bar Yosef
+ */
+
---
+++ set2/c9_pksc7padding.cpp
@@ -0,0 +1,41 @@
+/*
+ Cryptopals Crypto Challenges
+ Set 2
+ Challenge 9 - Implement PKCS#7 padding
+ Version 1.0
+
+
+ By: Guy Bar Yosef
+ */
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+
+void implementPKSC7(string & input, int blocksize); // ipmlements PKSC#7 padding
+
+/*
+int main () {
+    string input;
+    int block;
+    cin >> input >> block;
+    implementPKSC7(input, block);
+    return 0;
+}
+*/
+
+
+/*
+ *  Implements PKSC#7 padding for an inputted string, based on the inputed blocksize.
+ *  The padding works by finding the amount of bytes needed to make the input a multiple of the blocksize,
+ *  and then adding that value that amount of times to the end of the string.
+ *      EX (in hex): 03 03 03
+ *                   04 04 04 04 
+ */
+void implementPKSC7( string & input, int blocksize ) {
+    int padding_size = input.length() % blocksize;
+    string to_append = std::string((char)padding_size , padding_size);
+    input = input + to_append;
+}
